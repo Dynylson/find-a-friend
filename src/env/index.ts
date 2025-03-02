c26c24bcb4ece01a826cbae4aa93cbcd482dc5b3
Dyn Júnior <dynylsonjr@gmail.com>
2025-03-02 16:03:47 -0300

    feat: add new environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,10 @@ import 'dotenv/config'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
